b1cf5b1b53952155bbc528fb3c50d6c7c444fbad
hyperpostulate <mesutormanli@gmail.com>
2021-09-03 12:52:52 +0300

    refactors blink_sos.py

--- raspberry-pi-pico/blink_sos.py
+++ raspberry-pi-pico/blink_sos.py
@@ -2,22 +2,29 @@ from machine import Pin
 import utime
 led = Pin(25, Pin.OUT)
 
-def signal_S():
-    for i in range(3):
-        utime.sleep(0.25)
-        led.high()
-        utime.sleep(0.25)
-        led.low()
+def signal_short():
+    utime.sleep(0.25)
+    led.high()
+    utime.sleep(0.25)
+    led.low()
 
-def signal_O():
-    for i in range(3):
-        utime.sleep(0.5)
-        led.high()
-        utime.sleep(0.5)
-        led.low()
+def signal_long():
+    utime.sleep(0.5)
+    led.high()
+    utime.sleep(0.5)
+    led.low()
+    
+def signal_s():
+    for _ in range (3):
+        signal_short()
+
+def signal_o():
+    for _ in range (3):
+        signal_long()
 
 while True:
-    signal_S()
-    signal_O()
-    signal_S()
+    signal_s()
+    signal_o()
+    signal_s()
     utime.sleep(1)
+
