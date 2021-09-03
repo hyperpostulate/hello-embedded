6ae4e3c3325ad15472e7365df563244830efa19d
hyperpostulate <mesutormanli@gmail.com>
2021-09-03 13:23:47 +0300

    refactor

--- raspberry-pi-pico/src/pico/blink_sos.py
+++ raspberry-pi-pico/src/pico/blink_sos.py
@@ -1,6 +1,8 @@
 from machine import Pin
 import utime
+
 led = Pin(25, Pin.OUT)
+
 
 def signal_short():
     utime.sleep(0.25)
@@ -8,23 +10,26 @@ def signal_short():
     utime.sleep(0.25)
     led.low()
 
+
 def signal_long():
     utime.sleep(0.5)
     led.high()
     utime.sleep(0.5)
     led.low()
-    
+
+
 def signal_s():
-    for _ in range (3):
+    for _ in range(3):
         signal_short()
 
+
 def signal_o():
-    for _ in range (3):
+    for _ in range(3):
         signal_long()
+
 
 while True:
     signal_s()
     signal_o()
     signal_s()
     utime.sleep(1)
-
--- raspberry-pi-pico/src/pico/temperature.py
+++ raspberry-pi-pico/src/pico/temperature.py
@@ -2,10 +2,10 @@ import machine
 import utime
 
 sensor_temp = machine.ADC(4)
-conversion_factor = 3.3 / (65535)
+conversion_factor = 3.3 / 65535
 
 while True:
     reading = sensor_temp.read_u16() * conversion_factor
-    temp = 27 - (reading - 0.706)/0.001721
+    temp = 27 - (reading - 0.706) / 0.001721
     print(temp)
     utime.sleep(2)
